fe1c0901d1ece1cd7a67cb0d5999294239f1311d
Anton Krumbholz <anton.krumbholz@gmail.com>
2024-12-18 16:09:24 +0100

    .

--- determine.py
+++ determine.py
@@ -12,14 +12,14 @@ for img_path in imgs:
     determine_dat[pill_type]=determine_dat.get(pill_type,[])+[Image.open(img_path)]
 
 def determine(img,options:list|None=None):
-    if options ==None:
+    if options == None:
         options=determine_dat.keys()
     best_type="unknown"
     best_score=.006
     for test_type in options:
         if test_type not in determine_dat.keys():
-            raise NotADirectoryError(f"{test_type} does not exist at \"pills_by_folder/\".")
-        
+            raise NotADirectoryError(f"{test_type} does not exist at \"classify_data/\".")
+
         for test_img in determine_dat[test_type]:
             res=rmse(org_img=np.array(img), pred_img=np.array(test_img))
             if res<best_score:
--- orient_pills.py
+++ orient_pills.py
@@ -1,12 +1,7 @@
 import numpy as np
 from scipy import ndimage
 from PIL import Image
-
-def get_bounding_box(img_nd):
-    coords = np.argwhere(img_nd)
-    y_min, x_min = coords.min(axis=0)
-    y_max, x_max = coords.max(axis=0) + 1
-    return (x_min, y_min, x_max, y_max)
+from bounding_box import bounding_box
 
 def orient_pills(pills)->list[Image.Image]:
     pills_orientated=[]
@@ -16,16 +11,16 @@ def orient_pills(pills)->list[Image.Image]:
         def grade_rotation_angle(angle):
             rotated_mask = ndimage.rotate(mask, angle, reshape=True)
 
-            x_min, y_min, x_max, y_max=get_bounding_box(rotated_mask)
+            x_min, y_min, x_max, y_max=bounding_box(rotated_mask)
             return y_max-y_min
 
-
-        angle = min(range(0,360,10),key=grade_rotation_angle)
-        angle = min(range(angle-10,angle+10,1),key=grade_rotation_angle)
+        angle = min(range(0,180,45),key=grade_rotation_angle)
+        angle = min(range(angle-30,angle+40,10),key=grade_rotation_angle)
+        angle = min(range(angle-5,angle+6,1),key=grade_rotation_angle)
 
         rotated_mask = ndimage.rotate(mask, angle, reshape=True)
-        rotated_pill=pill.rotate(angle, expand=True).crop(get_bounding_box(rotated_mask))
-        x_min, y_min, x_max, y_max=get_bounding_box(rotated_mask)
+        rotated_pill=pill.rotate(angle, expand=True).crop(bounding_box(rotated_mask))
+        x_min, y_min, x_max, y_max=bounding_box(rotated_mask)
         new=Image.new("RGB",(1000,1000))
         new.paste(rotated_pill,(int(500-(x_max-x_min)/2),int(500-(y_max-y_min)/2)))
         pills_orientated.append(new)
