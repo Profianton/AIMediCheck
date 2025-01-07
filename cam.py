805f444d9e52abb8571270a0f1ffbd6f24ee8ec9
Anton Krumbholz <anton.krumbholz@gmail.com>
2025-01-07 17:59:24 +0100

    docu

--- cam.py
+++ cam.py
@@ -10,7 +10,7 @@ picam2 = Picamera2()
 capture_config = picam2.create_still_configuration()
 picam2.start()
 #picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 11.44}) #Fokus setzen
-picam2.set_controls({"AfMode": controls.AfModeEnum.Auto}) #Fokus setzen
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous}) #Fokus setzen
 
 def capture():
     """Bild aufnehmen und zuschneiden
@@ -19,5 +19,5 @@ def capture():
         Image.Image: Aufgenommenes und zugeschnittenes Bild
     """
     image:Image.Image = picam2.switch_mode_and_capture_image(capture_config)
-    image=image.crop((1000,120,3600,2592))
+    image=image.crop((1125,280,3490,2580))
     return image
--- main.py
+++ main.py
@@ -4,20 +4,33 @@ from glob import glob
 import light
 from button import on_button_press
 from client import analyse
+import rgb_led
 @on_button_press
+def capture_and_analyse():
+    rgb_led.set_led([rgb_led.Color.BLUE,rgb_led.Color.BLACK])
+    image=capture()
+    res=analyse(image)
+    print(res)
+    if res["match"]:
+        rgb_led.set_led(rgb_led.Color.GREEN)
+    else:
+        rgb_led.set_led(rgb_led.Color.RED)
+
 def capture_and_save()->Image.Image:
     print("capturing")
+    rgb_led.set_led(rgb_led.Color.YELLOW)
     global image
     path=f"images/{len(glob('images/*'))}.png"
     image=capture()
     image.save(path, compress_level=1)
+    rgb_led.set_led(rgb_led.Color.GREEN)
     return image
+
 while True:
     match input("cmd: "):
-        case "pwm":
-            light.high = float(input("Value (0-100): "))
-            light.on()
         case "analyse":
             print(analyse(image))
-        case _:
+        case "s":
             capture_and_save()
+        case _:
+            capture_and_analyse()
